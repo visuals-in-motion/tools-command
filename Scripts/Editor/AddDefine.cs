3440fd4dd5ec86639d8a544e9842483743697f73
Оля Набиева <olya-nabieva@mail.ru>
2021-07-22 12:47:16 +0300

    добавление директивы

---
+++ Scripts/Editor/AddDefine.cs
@@ -0,0 +1,22 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Visuals
+{
+#if UNITY_EDITOR
+    [InitializeOnLoad]
+    public class AddDefine
+    {
+        static AddDefine()
+        {
+            string symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone);
+
+            if (!symbols.Contains("COMMAND"))
+            {
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, symbols + ";COMMAND");
+            }
+        }
+    }
+#endif
+}
