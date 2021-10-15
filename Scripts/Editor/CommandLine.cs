a8ded42485634b40ee119daa7c0942af36e2ad44
Tech <tech@visuals.ru>
2021-10-15 12:53:30 +0300

    Update CommandLine.cs

--- Scripts/Editor/CommandLine.cs
+++ Scripts/Editor/CommandLine.cs
@@ -8,13 +8,19 @@ namespace Visuals
     {
 		public static string Run(string command)
 		{
-#if UNITY_EDITOR_WIN
 			var processInfo = new ProcessStartInfo();
 			processInfo.RedirectStandardOutput = true;
 			processInfo.WindowStyle = ProcessWindowStyle.Hidden;
 			processInfo.CreateNoWindow = true;
 			processInfo.UseShellExecute = false;
+
+#if UNITY_EDITOR_WIN
 			processInfo.FileName = "cmd.exe";
+#else
+#if UNITY_EDITOR_OSX
+			processInfo.FileName = "/bin/bash";
+#endif
+#endif
 			processInfo.WorkingDirectory = Application.dataPath.Replace("/Assets", "");
 			processInfo.Arguments = "/c " + command;
 
@@ -22,9 +28,6 @@ namespace Visuals
 			string output = process.StandardOutput.ReadToEnd();
 			process.Close();
 			return output;
-#else
-			return String.Empty;
-#endif
 		}
 	}
 }
