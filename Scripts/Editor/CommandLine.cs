a29213d9c3db42e0e71537fcb5ce82691fe644b0
Vacuoll <52681127+Vacuoll@users.noreply.github.com>
2022-06-02 12:26:24 +0300

    Update CommandLine.cs

--- Scripts/Editor/CommandLine.cs
+++ Scripts/Editor/CommandLine.cs
@@ -22,16 +22,9 @@ namespace Visuals
 			processInfo.Arguments = $"-c \" {command} \"";
 #endif
 			processInfo.WorkingDirectory = (string.IsNullOrEmpty(workingDirectory)) ? Application.dataPath.Replace("/Assets", "") : workingDirectory;
-#if UNITY_STANDALONE_WIN || UNITY_WSA || UNITY_WSA_10_0
-			UnityEngine.Debug.LogError("Command: " + command);
-			UnityEngine.Debug.LogError("WorkingDirectory: " + processInfo.WorkingDirectory);
-#endif
-			
+		
 			var process = Process.Start(processInfo);
 			string output = (enableOutput) ? process.StandardOutput.ReadToEnd() : string.Empty;
-#if UNITY_STANDALONE_WIN || UNITY_WSA || UNITY_WSA_10_0
-			UnityEngine.Debug.LogError("output: " + output);
-#endif
 
 			process.Close();
 			return output;
